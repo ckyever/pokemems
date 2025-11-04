defbb993cf97e2f6c0d9fdac26e655b9cfc37db6
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-04 16:59:48 +1100

    Add tall grass and randomly place pokemon behind them

--- src/components/TallGrass.jsx
+++ src/components/TallGrass.jsx
@@ -1,12 +1,38 @@
 import { useState, useEffect } from "react";
 
 import "../styles/TallGrass.css";
+import tallGrassSprite from "../assets/tall-grass.png";
+
+function createGrassWithPokemon(pokemonList, numberOfTallGrass) {
+  // Create an array of undefined elements representing grass
+  const grassList = Array.from({ length: numberOfTallGrass });
+
+  // Keep track of free index to place a pokemon
+  const availableIndexes = Array.from(
+    { length: numberOfTallGrass },
+    (_, index) => index
+  );
+
+  // Randomly place each pokemon into the grass
+  pokemonList.forEach((pokemon) => {
+    const randomIndex = Math.floor(Math.random() * availableIndexes.length);
+    const grassIndex = availableIndexes.splice(randomIndex, 1);
+    grassList[grassIndex] = {
+      pokemonName: pokemon.name,
+      pokemonSprite: pokemon.sprite,
+    };
+  });
+
+  return grassList;
+}
 
 function TallGrass({ pokemonList, isWinner, setIsWinner, incrementScore }) {
   const [clickedPokemon, setClickedPokemon] = useState([]);
+  const [grassList, setGrassList] = useState([]);
 
   useEffect(() => {
     setClickedPokemon([]);
+    setGrassList(createGrassWithPokemon(pokemonList, 25));
   }, [pokemonList]);
 
   function handlePokemonClick(pokemonKey) {
@@ -26,15 +52,26 @@ function TallGrass({ pokemonList, isWinner, setIsWinner, incrementScore }) {
 
   return (
     <div className="tall-grass">
-      {pokemonList.map((pokemon) => (
-        <img
-          key={pokemon.name}
-          src={pokemon.sprite}
-          onClick={() => {
-            handlePokemonClick(pokemon.name);
-          }}
-        />
-      ))}
+      {grassList &&
+        grassList.map((grass, index) => (
+          <div key={index} className="grass">
+            {grass != null ? (
+              <img
+                className="pokemon"
+                src={grass.pokemonSprite}
+                alt={grass.pokemonName + " sprite"}
+                onClick={() => {
+                  handlePokemonClick(grass.pokemonName);
+                }}
+              />
+            ) : undefined}
+            <img
+              className="tall-grass-sprite"
+              src={tallGrassSprite}
+              alt="tall grass"
+            />
+          </div>
+        ))}
     </div>
   );
 }
